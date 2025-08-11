97eba7a45d689ab64162a0319b8647dc29e703fc
jcbarr81 <95983545+jcbarr81@users.noreply.github.com>
2025-08-11 18:21:52 -0400

    Set splash screen logo as window background

--- ui/splash_screen.py
+++ ui/splash_screen.py
@@ -11,14 +11,19 @@ class SplashScreen(QWidget):
         super().__init__()
         self.setWindowTitle("UBL")
 
+        self.setObjectName("splash_screen")
+
         layout = QVBoxLayout()
         layout.addStretch()
 
         logo_path = Path(__file__).resolve().parents[1] / "logo" / "UBL.png"
         logo_url = logo_path.as_posix()
         self.setStyleSheet(
-            f"background-image: url('{logo_url}');"
-            "background-repeat: no-repeat; background-position: center;"
+            f"""
+            #splash_screen {{
+                border-image: url('{logo_url}') 0 0 0 0 stretch stretch;
+            }}
+            """
         )
 
         self.start_button = QPushButton("Start Game")
